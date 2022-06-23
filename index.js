b3fc94cc0876c1aade3499a9af23cf17b49b1aa3
gabs <gabriellerrc@gmail.com>
2022-06-23 04:04:16 -0300

    DB connected

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-import express from "express";
+import express, {json} from "express";
 import dotenv from "dotenv";
+import {MongoClient} from "mongodb";
+import joi from "joi";
 
 dotenv.config()
 
 const app = express();
+app.use(json())
 const port = process.env.PORTA;
 
-app.listen(port, () => {
-    console.log(`Servidor up ${port}`)
+let db;
+const mongoClient = new MongoClient(process.env.MONGO_URL)
+
+const promise = mongoClient.connect();
+promise.then(() => {
+    db = mongoClient.db(process.env.BANCO);
+    console.log("DB conectado")
+});
+
+promise.catch((e) => console.log("Error", e))
+
+app.post("/participants", (req, res) => {
+    const participant = req.body;
+    const participantSchema = joi.object({name: joi.string().required()});
+    participantSchema.validate(participant);
 })
+
+
+app.listen(port, () => {
+    console.log(`Servidor up porta ${port}`)
+});
